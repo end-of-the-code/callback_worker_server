a4b6baf7074f9f189e72e4da7318c106447a1e1e
k9want <zndwlrnjr26@daum.net>
2025-11-20 14:05:53 +0900

    rename: CallbackKafkaConsumer로 클래스명 변경
    -이전 클래스명 : CallbackWorkerKafkaConsumer

--- src/main/java/com/example/callback/worker/infrastructure/queue/kafka/CallbackKafkaConsumer.java
+++ src/main/java/com/example/callback/worker/infrastructure/queue/kafka/CallbackKafkaConsumer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class CallbackWorkerKafkaConsumer {
+public class CallbackKafkaConsumer {
 
   private final CallbackWorkerService callbackWorkerService;
 
